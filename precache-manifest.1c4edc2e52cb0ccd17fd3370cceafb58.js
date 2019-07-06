543ae92e75e272b540801fee176b60a14f85eeef
Ray Dai <ray.dai@agridigital.io>
2019-07-06 17:53:59 +1000

    Updates

---
+++ precache-manifest.1c4edc2e52cb0ccd17fd3370cceafb58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6062642aa4f8f5e3da04f342c6af67da",
+    "url": "/seven-tile/index.html"
+  },
+  {
+    "revision": "9538f7483f596a382ff3",
+    "url": "/seven-tile/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "2dd3d8718e7a3a9f59b2",
+    "url": "/seven-tile/static/js/2.cfb64bb0.chunk.js"
+  },
+  {
+    "revision": "9538f7483f596a382ff3",
+    "url": "/seven-tile/static/js/main.8c4590ca.chunk.js"
+  },
+  {
+    "revision": "45b5833b0f80f3f50e0d",
+    "url": "/seven-tile/static/js/runtime~main.08c98c03.js"
+  },
+  {
+    "revision": "2a3f011a74b71cac77f0134976aad150",
+    "url": "/seven-tile/static/media/logo.2a3f011a.png"
+  },
+  {
+    "revision": "25b4e634dae07d5d77ebe67a3d0743b2",
+    "url": "/seven-tile/static/media/tile.25b4e634.jpg"
+  }
+]);
